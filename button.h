e83f18ade599f0bd9f5794c0c01ac304b9575946
So hyeongjun <lego0120@naver.com>
2023-12-07 13:00:19 +0900

    BUTTON-라이브러리 만들기

---
+++ button.h
@@ -0,0 +1,20 @@
+// button.h
+
+#ifndef _BUTTON_H_
+#define _BUTTON_H_
+
+#define MESSAGE_ID 1122
+typedef struct
+{
+    long int messageNum;
+    int keyInput;
+    int pressed;
+} BUTTON_MSG_T;
+
+void* buttonThFunc(void*arg);
+int probeButtonPath(char *newPath);
+int buttonInit(void);
+int buttonExit(void);
+
+#endif
+
